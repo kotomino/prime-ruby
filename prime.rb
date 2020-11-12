6cf14d45fc908626126010fefd6e5ec84b8fdcae
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2020-11-12 17:12:06 -0500

    commit tree 3194724a4c09ab74f3e4d2f146e4e4a2fd2be699

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+    if num <= 1 
+        false
+    else
+        (2.. (num - 1)).to_a.all? do |divisor|
+            num % divisor != 0
+        end
+    end
+end
+
